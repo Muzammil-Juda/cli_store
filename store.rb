eff6afde5af1462643ef9442c18dd48c4f10b36e
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2024-01-17 06:28:04 +0000

    Done but one thing is left to do i.e stop after exit

--- store.rb
+++ store.rb
@@ -1,6 +1,6 @@
 require_relative "department"
 
-class Store
+class Store < Department
   def initialize
     @departments = []
   end
@@ -12,16 +12,32 @@ class Store
       puts "Welcome to the CLI Store!"
       puts "*************************"
       puts ""
-      puts "1. View all departments"
-      puts "2. Exit"
+      puts "Visit any department:"
+      puts "1. Newborn"
+      puts "2. Girls"
+      puts "3. Boys"
+      puts "4. Women"
+      puts "5. Men"
+      puts "6. Home"
+      puts "7. Exit"
       puts ""
-      puts "Choose an option:"
+      puts "Enter the number of your desired Department:"
       option = gets.chomp.to_i
 
       case option
       when 1
-        department
+        new_born
       when 2
+        girls
+      when 3
+        boys
+      when 4
+        women
+      when 5
+        men
+      when 6
+        home
+      when 7
         exit
       else
         puts "Invalid option! Please try again."
@@ -29,23 +45,48 @@ class Store
     end
   end
 
- 
-
   private
 
-  def department
-    new_born = Department.new("New Born", "I1")
-    girls = Department.new("Girls", "I2")
-    boys = Department.new("Boys", "I3")
-    women = Department.new("Women", "I4")
-    men = Department.new("Men", "I5")
-    home = Department.new("Home", "I6")
+  def new_born
+    n_b = Store.new
+    n_b.name = "Newborn"
+    n_b.aisle = "I1"
+    puts n_b.to_s
+  end
 
-    @departments.push(new_born, girls, boys, women, men, home)
+  def girls
+    grl = Store.new
+    grl.name = "Girls"
+    grl.aisle = "I2"
+    puts grl.to_s
+  end
 
-    @departments.each do|department|
-      pp department
-    end
+  def boys
+    boy = Store.new
+    boy.name = "Boys"
+    boy.aisle = "I3"
+    puts boy.to_s
+  end
+
+  def women
+    wmn = Store.new
+    wmn.name = "Women"
+    wmn.aisle = "I4"
+    puts wmn.to_s
+  end
+
+  def men
+    mn = Store.new
+    mn.name = "Men"
+    mn.aisle = "I5"
+    puts mn.to_s
+  end
+  
+  def home
+    hme = Store.new
+    hme.name = "Home"
+    hme.aisle = "I6"
+    puts hme.to_s
   end
 
   def exit
